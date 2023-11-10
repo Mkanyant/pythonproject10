da2af56e954c7cc746da9d7ef7ebe94334725ad8
Mkanyanat <s6519410049@sau.ac.th>
2023-11-10 11:58:03 +0700

    code v.1 :

---
+++ test06.py
@@ -0,0 +1,12 @@
+f_iot = open('iot4.txt', 'r' ,encoding='utf-8')
+
+data = f_iot.readline()
+print(data)
+data = f_iot.readline()
+print(data)
+data = f_iot.readline()
+print(data)
+data = f_iot.readline()
+print(data)
+
+f_iot.close
